f81853c91f9a4ad6eb6f5fed9f2ee2de943d7e4e
Tate Keller <tatek1993@gmail.com>
2020-08-07 11:53:21 -0500

    ex3 tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,35 @@
+# We're given a list of lists that contain integers:
+# And we need to compute the numbers that exist in all lists.
+
+# we need to know the length of list
+# create cache and result array
+
+# for each num in list[i] -> append to cache -> cache[num] = num
+## if num already in cache -> cache[num] += 1
+
+# for each num in cache
+## if count == list.length -> append to result
+
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    result = []
+
+    for array in arrays:
+        for num in array:
+            if num not in cache:
+                cache[num] = 1
+            else:
+                cache[num] += 1
+
+    for num in cache:
+        if cache[num] == len(arrays):
+            result.append(num)
 
     return result
 
