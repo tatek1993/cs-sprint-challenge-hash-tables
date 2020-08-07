dcd4c659d3fb07b2fa7593877520706102e934a9
Tate Keller <tatek1993@gmail.com>
2020-08-07 11:08:17 -0500

    ex5 tests passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,36 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
 
+    # create a cache
+    cache = {}
+    # create array to hold final result
+    result = []
+
+    # loop through queries, if not in cache add it
+    for query in queries:
+        if query not in cache:
+            cache[query] = query
+
+    # split file names by /
+    # loop through file names
+
+    for file in files:
+        split_file = file.split('/')
+        # if we find part of the file name in cache, we add it to result
+        for frag in split_file:
+            if frag in cache:
+                result.append(file)
+
     return result
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+    files = ['/bin/foo', '/bin/bar', '/usr/bin/baz']
+    queries = ["foo", "qux", "baz"]
     print(finder(files, queries))
